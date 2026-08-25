37e89b19c31b3f955e9bf44abdac166e26d2b9e3
ismail8796940 <gujjrigujjar4@gmail.com>
2026-08-25 03:16:29 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1044,62 +1044,7 @@ def webhook():
         )
 
         return "OK", 200
-    status_state = user_status_state.get(chat_id)
-
-        if status_state == "WAITING_USERNAME":
-            if text == "إلغاء":
-                user_status_state.pop(chat_id, None)
-
-                show_users_menu(chat_id)
-                return "OK", 200
-
-            target_user = get_user_by_username(text)
-
-            if not target_user:
-                send_message(
-                    chat_id,
-                    "اسم المستخدم غير موجود.\n"
-                    "أدخل اسم المستخدم من جديد:"
-                )
-                return "OK", 200
-
-            current_status = str(
-                target_user.get("status", "")
-            ).strip().upper()
-
-            if current_status == "ACTIVE":
-                new_status = "INACTIVE"
-            else:
-                new_status = "ACTIVE"
-
-            result = set_user_status(
-                target_user.get("username"),
-                new_status
-            )
-
-            if not result.get("ok"):
-                send_message(
-                    chat_id,
-                    "تعذر تعديل حالة المستخدم."
-                )
-                return "OK", 200
-
-            user_status_state.pop(chat_id, None)
-
-            if new_status == "ACTIVE":
-                new_status_ar = "فعال"
-            else:
-                new_status_ar = "غير فعال"
-
-            send_message(
-                chat_id,
-                "تم تعديل حالة المستخدم بنجاح.\n\n"
-                f"اسم المستخدم: {target_user.get('username')}\n"
-                f"الحالة الجديدة: {new_status_ar}"
-            )
-
-            show_users_menu(chat_id)
-            return "OK", 200
+  
 
      if __name__ == "__main__":
     port = int(
